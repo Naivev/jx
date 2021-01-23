627c4d13a545dabd89600b25ff29a041f5fa1715
Naivev <45974159+Naivev@users.noreply.github.com>
2021-01-23 13:33:23 +0800

    Update jd_family.js

--- jd_family.js
+++ jd_family.js
@@ -17,6 +17,7 @@ if ($.isNode()) {
     cookiesArr.push(jdCookieNode[item])
   })
   if (process.env.JD_DEBUG && process.env.JD_DEBUG === 'false') console.log = () => {
+  if (process.env.FAM) process.env.FAM.split('&').map((item, index) => Number(item) === 0 ? cookiesArr = [] : cookiesArr.splice(Number(item) - 1 - index, 1))
   };
 } else {
   let cookiesData = $.getdata('CookiesJD') || "[]";
