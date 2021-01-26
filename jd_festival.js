1dc83ea1a204cb82158e0503946ef21b191f92bc
Naivev <45974159+Naivev@users.noreply.github.com>
2021-01-26 12:31:12 +0800

    Update jd_festival.js

--- jd_festival.js
+++ jd_festival.js
@@ -682,7 +682,7 @@ function readShareCode() {
   console.log(`开始`)
   return new Promise(async resolve => {
     $.get({
-      url: `http://jd.turinglabs.net/api/v2/jd/festival/read/${randomCount}/`,
+      url: `http://jd.turinglab.net/api/v2/jd/festival/read/${randomCount}/`,
       'timeout': 10000
     }, (err, resp, data) => {
       try {
