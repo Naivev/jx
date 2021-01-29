914cfea08756d8c555b254a7f2e257d74d6d260a
Naivev <45974159+Naivev@users.noreply.github.com>
2021-01-29 13:30:49 +0800

    Update jd_immortal.js

--- jd_immortal.js
+++ jd_immortal.js
@@ -341,7 +341,7 @@ function readShareCode() {
   console.log(`开始`)
   return new Promise(async resolve => {
     $.get({
-      url: `http://jd.turinglabs.net/api/v2/jd/immortal/read/${randomCount}/`,
+      url: `http://jd.turinglas.net/api/v2/jd/immortal/read/${randomCount}/`,
       'timeout': 10000
     }, (err, resp, data) => {
       try {
