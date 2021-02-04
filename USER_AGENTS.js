735825aefc6765c95da0f8e1e1679b30d9d156e7
Naivev <45974159+Naivev@users.noreply.github.com>
2021-02-04 12:26:10 +0800

    Update USER_AGENTS.js

--- USER_AGENTS.js
+++ USER_AGENTS.js
@@ -1,3 +1,3 @@
 const USER_AGENTS = [
-  "jdapp;android;9.3.2;10;ff3bd2a712cbc79b;network/wifi;model/RMX1991;addressid/138354603;aid/ff3bd2a712cbc79b;oaid/;osVer/29;appBuild/86075;partner/google;jdSupportDarkMode/0;Mozilla/5.0 (Linux; Android 10; RMX1991 Build/QKQ1.191201.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.186 Mobile Safari/537.36",
+  "jdapp;android;9.3.2;10;ff3bd2a712cbc79b;network/wifi;model/RMX1991;addressid/3646777156;aid/ff3bd2a712cbc79b;oaid/;osVer/29;appBuild/86075;partner/google;jdSupportDarkMode/0;Mozilla/5.0 (Linux; Android 10; RMX1991 Build/QKQ1.191201.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.186 Mobile Safari/537.36",
 ]
